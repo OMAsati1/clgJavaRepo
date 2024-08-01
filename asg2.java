1a42985c4c8146dc0ff7ddfbbe105670ddddb24d
OMAsati1 <omasati@gmail.com>
2024-08-01 12:31:06 +0530

    2nd assignment

---
+++ asg2.java
@@ -0,0 +1,13 @@
+public class asg2 {
+    public static void main(String[] args) {
+        int a = 10;
+        short b = 20;
+        long c = 1000000L;
+        byte d = 5;
+
+        System.out.println("int a = " + a);
+        System.out.println("short b = " + b);
+        System.out.println("long c = " + c);
+        System.out.println("byte d = " + d);
+    }
+}
